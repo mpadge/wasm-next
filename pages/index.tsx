b429c358cd3d1562392fc77bd0dd2eabfafc8adf
mpadge <mark.padgham@email.com>
2023-11-24 12:00:27 +0100

    rm former calls to add_one fn

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,13 @@
 import { useRouter } from 'next/router'
 import { useState } from 'react';
 import Link from 'next/link'
-import RustComponent from '../components/RustComponent'
 import RustComponent2 from '../components/RustComponent2'
 import Control from '../components/control'
 
 
 export default function Page() {
-    const { query } = useRouter()
-        const number1 = parseInt(query.number as string) || 30
-        const number2 = 10
-
-        const [Number1, setNumber1] = useState(number1);
-    const [Number2, setNumber2] = useState(number2);
+    const [Number1, setNumber1] = useState(10);
+    const [Number2, setNumber2] = useState(10);
     const handleNumber1Change = (value: string) => {
         setNumber1(value);
     }
@@ -29,9 +24,7 @@ export default function Page() {
             handleNumber1Change={setNumber1}
             handleNumber2Change={setNumber2}
         />
-        <RustComponent number={number1} />
-        <Link href={`/?number=${number1 + 1}`}>+</Link>
-        <RustComponent2 number1={number1} number2={number2} />
+        <RustComponent2 number1={Number1} number2={Number2} />
         </div>
         </>
     )
