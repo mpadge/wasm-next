b77123a40a0deb8e32be18970ad369f157948ffb
mpadge <mark.padgham@email.com>
2023-11-27 13:38:33 +0100

    rm code comments

--- components/RustComponent2.tsx
+++ components/RustComponent2.tsx
@@ -8,12 +8,10 @@ interface RustComponent2Props {
 
 const RustComponent2 = dynamic({
   loader: async () => {
-    // Import the wasm module
     // @ts-ignore
     const exports = (await import('../add.wasm')) as AddModuleExports
     const { add_two: addTwo } = exports
 
-    // Return a React component that calls the add_two method on the wasm module
     return ({ number1, number2 }: RustComponent2Props) => (
       <div>
         <>{addTwo(number1, number2)}</>
