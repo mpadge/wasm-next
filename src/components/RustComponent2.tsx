2cbecba446aa41d6a3971a05dbec2f4ea2a7aac4
mpadge <mark.padgham@email.com>
2023-11-28 10:40:42 +0100

    mv root to src; rename other dirs

--- src/components/RustComponent2.tsx
+++ src/components/RustComponent2.tsx
@@ -1,4 +1,4 @@
-import type { AddModuleExports } from '../wasm'
+import { initSync, InitOutput } from '../wasm/testcrate.js';
 import dynamic from 'next/dynamic'
 
 interface RustComponent2Props {
@@ -9,7 +9,7 @@ interface RustComponent2Props {
 const RustComponent2 = dynamic({
   loader: async () => {
     // @ts-ignore
-    const exports = (await import('../wasm/add.wasm')) as AddModuleExports
+    const exports = (await import('../wasm/testcrate_bg.wasm')) as AddModuleExports
     const { add_two: addTwo } = exports
 
     return ({ number1, number2 }: RustComponent2Props) => (
--- wasm.d.ts
+++
@@ -1,3 +0,0 @@
-export interface AddModuleExports {
-  add_two(number1: Number, number2: Number): Number
-}
