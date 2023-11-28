c43a64cbca3ced65e194508e2a433b919b9c091d
mpadge <mark.padgham@email.com>
2023-11-28 10:56:25 +0100

    update import paths in rustcomponent2.tsx

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     // Fixes `Error occurred prerendering page "/"`
     config.output.webassemblyModuleFilename =
       isServer && !dev
-        ? '../static/wasm/[modulehash].wasm'
-        : 'static/wasm/[modulehash].wasm'
+        ? '../static/pkg/[modulehash].wasm'
+        : 'static/pkg/[modulehash].wasm'
 
     // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
     config.experiments = { ...config.experiments, asyncWebAssembly: true }
--- src/components/RustComponent2.tsx
+++ src/components/RustComponent2.tsx
@@ -1,4 +1,7 @@
-import { initSync, InitOutput } from '../wasm/testcrate.js';
+import type { InitOutput } from '@/../pkg/testcrate';
+import { initSync } from '@/../pkg/testcrate.js';
+import { add_two } from '@/../pkg/testcrate_bg.wasm';
+
 import dynamic from 'next/dynamic'
 
 interface RustComponent2Props {
@@ -9,7 +12,7 @@ interface RustComponent2Props {
 const RustComponent2 = dynamic({
   loader: async () => {
     // @ts-ignore
-    const exports = (await import('../wasm/testcrate_bg.wasm')) as AddModuleExports
+    const exports = (await import('@/../pkg/testcrate_bg.wasm')) as AddModuleExports
     const { add_two: addTwo } = exports
 
     return ({ number1, number2 }: RustComponent2Props) => (
