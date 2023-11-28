a895066263ecd4620a43e42405f21c37e0ff0407
mpadge <mark.padgham@email.com>
2023-11-28 12:56:57 +0100

    fix call to mult_two fn

--- src/components/RustComponent.tsx
+++ src/components/RustComponent.tsx
@@ -1,7 +1,5 @@
 import type { InitOutput } from '@/../pkg/testcrate';
 import { initSync } from '@/../pkg/testcrate.js';
-import { add_two } from '@/../pkg/testcrate_bg.wasm';
-
 import dynamic from 'next/dynamic'
 
 interface RustComponentProps {
@@ -18,14 +16,38 @@ const RustComponent = dynamic({
     // @ts-ignore
     const { mult_two: multTwo } = (await import('@/../pkg/testcrate_bg.wasm')) as MultTwoExports
 
-    return ({ vector1, vector2 }: RustComponentProps) => (
-      <div>
-        <>{multTwo(vector1, vector2)}</>
-      </div>
+    const { mult_two, get_result_len, memory } = await import('@/../pkg/testcrate_bg.wasm');
+
+    // Initialize your input vectors
+    const vector1 = Float64Array.from({ length: 5 }, () => Math.random());
+    const vector2 = Float64Array.from({ length: 5 }, () => Math.random());
+
+    // Allocate space in the Wasm memory for the input vectors and copy the vectors into it
+    const ptr1 = allocateSpaceForVector(vector1);
+    const ptr2 = allocateSpaceForVector(vector2);
+
+    // Call mult_two and get the pointer to the result vector
+    const resultPtr = mult_two(ptr1, vector1.length, ptr2, vector2.length);
+
+    // Call get_result_len to get the length of the result vector
+    const resultLen = get_result_len();
+
+    // Create a new Float64Array view on the Wasm memory
+    const resultVector = new Float64Array(memory.buffer, resultPtr, resultLen);
+
+    // Now resultVector is a JavaScript Float64Array containing the result
+    console.log(resultVector);
+
+    return (
+        <div>
+          {resultVector.map((value, index) => (
+            <div key={index}>{value}</div>
+          ))}
+        </div>
     )
-  },
-  // Ensure only client-side execution:
-  ssr: false
+    },
+    // Ensure only client-side execution:
+    ssr: false
 })
 
 export default RustComponent
