b345986ac31be98e34d3151b64a9c77779b98d8d
mpadge <mark.padgham@email.com>
2023-11-30 15:26:07 +0100

    add loadData react effect

--- src/components/WasmJson.tsx
+++ src/components/WasmJson.tsx
@@ -1,29 +1,50 @@
 import type { InitOutput } from '@/../pkg/testcrate';
 import { initSync } from '@/../pkg/testcrate.js';
+
 import dynamic from 'next/dynamic'
+import { useEffect, useState} from 'react';
 
 import styles from '../styles/styles.module.css';
 
+interface JsonProps {
+  filename1: string
+  filename2: string
+  varname: string
+  nentries: number
+}
+
 const WasmJson = dynamic({
   loader: async () => {
-    // @ts-ignore
-    // const { mult_json: multJson } = (await import('@/../pkg/testcrate_bg.wasm')) as MultTwoExports
-
-    // const { mult_json, get_result_len, memory } = await import('@/../pkg/testcrate_bg.wasm');
-
     const Component = ({ filename1, filename2, varname, nentries }: JsonProps) => {
+      const [data1, setData1] = useState(null);
+      const [data2, setData2] = useState(null);
 
-        return (
-            <div className={styles.vector}>
-              <h1>Json Result</h1>
-            </div>
-        )
+      useEffect(() => {
+        const loadData = async () => {
+          const response1 = await fetch(filename1);
+          const json1 = await response1.json();
+          setData1(json1);
+
+          const response2 = await fetch(filename2);
+          const json2 = await response2.json();
+          setData2(json2);
+        };
+
+        loadData();
+      }, [filename1, filename2]);
+
+      // TODO: Pass data1 and data2 to your wasm function
+
+      return (
+        <div className={styles.json}>
+          <h1>Json Result</h1>
+        </div>
+      )
     }
 
     return Component
-    },
-    // Ensure only client-side execution:
-    ssr: false
-})
+  },
+  ssr: false
+});
 
 export default WasmJson
