263c45d42ac556b504abd2f861cced68d953ec16
mpadge <mark.padgham@email.com>
2023-12-08 14:33:01 +0100

    connect actual component

--- src/components/WasmBindGen2.tsx
+++ src/components/WasmBindGen2.tsx
@@ -1,9 +1,4 @@
-import * as wasm_js from '@/../pkg/testcrate.js';
-
 import dynamic from 'next/dynamic'
-import { useEffect, useState} from 'react';
-
-import styles from '@/styles/styles.module.css';
 
 export interface AddModuleExports {
     parse_json(): string
@@ -18,57 +13,21 @@ interface BindGenProps {
     handleResultChange: (Object: any) => void
 }
 
-const WasmBindGenCalc2 = dynamic({
-    loader: async () => {
-        const Component = ({ filename1, filename2, varnames, nentries, bindgenResult, handleResultChange }: BindGenProps) => {
-            const [data1, setData1] = useState(null);
-            const [data2, setData2] = useState(null);
-
-            useEffect(() => {
-                const loadData = async () => {
-                    const response1 = await fetch(filename1);
-                    const json1 = await response1.json();
-                    setData1(json1);
-
-                    const response2 = await fetch(filename2);
-                    const json2 = await response2.json();
-                    setData2(json2);
-                };
-
-                loadData();
-                }, [filename1, filename2]);
-
-            useEffect(() => {
-                fetch('@/../pkg/testcrate_bg.wasm')
-                .then(response => {
-                    return response.arrayBuffer();
-                    })
-                .then(bytes => {
-                    if (data1 && data2) {
-                        const wasm_binary = wasm_js.initSync(bytes);
-                        const varname = varnames.join(",");
-                        const resultJson = wasm_js.parse_json(JSON.stringify(data1), JSON.stringify(data2), varname, nentries);
-                        const resultObj = JSON.parse(resultJson);
-                        handleResultChange(resultObj);
-                    }
-                    })
-                .catch(error => {
-                    console.error('Error fetching wasm module:', error);
-                    });
-                }, [data1, data2, varnames, nentries, handleResultChange]);
-
-            return (
-                <div className={styles.json2}>
-                    <h1>BindGen2</h1>
-                        {bindgenResult && <pre>{JSON.stringify(bindgenResult, null, 2)}</pre>}
-                </div>
-            )
-
-        }
-
-        return Component
-    },
+const BindGenReactComponent = dynamic(() => import('@/components/BindGenComponent'), {
     ssr: false
 });
 
-export default WasmBindGenCalc2;
+export default function WasmBindGen2 (props: BindGenProps) {
+    return (
+        <>
+        <BindGenReactComponent
+            filename1={props.filename1}
+            filename2={props.filename2}
+            varnames={props.varnames}
+            nentries={props.nentries}
+            bindgenResult={props.bindgenResult}
+            handleResultChange={props.handleResultChange}
+        />
+        </>
+    );
+};
