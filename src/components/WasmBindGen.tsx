4c8e48fb457bc13dd40bb6216dc6060af4d0f542
mpadge <mark.padgham@email.com>
2023-12-14 11:44:54 +0100

    refactor WasmBindGen as client-side component

--- src/components/WasmBindGen.tsx
+++ src/components/WasmBindGen.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client'
 
 // A self-contained nested component with:
 // - A nextjs dynamic loader, and
@@ -24,59 +24,55 @@ interface BindGenProps {
     nentries: number
 }
 
-const WasmBindGenCalc = dynamic({
-    loader: async () => {
-        const Component = (props: BindGenProps) => {
-            const [data1, setData1] = useState(null);
-            const [data2, setData2] = useState(null);
-            const [result, setResult] = useState<Object | null>(null);
+const Component = (props: BindGenProps) => {
+    const [data1, setData1] = useState(null);
+    const [data2, setData2] = useState(null);
+    const [result, setResult] = useState<Object | null>(null);
 
-            useEffect(() => {
-                const loadData = async () => {
-                    const response1 = await fetch(props.filename1);
-                    const json1 = await response1.json();
-                    setData1(json1);
+    useEffect(() => {
+        const loadData = async () => {
+            const response1 = await fetch(props.filename1);
+            const json1 = await response1.json();
+            setData1(json1);
 
-                    const response2 = await fetch(props.filename2);
-                    const json2 = await response2.json();
-                    setData2(json2);
-                };
+            const response2 = await fetch(props.filename2);
+            const json2 = await response2.json();
+            setData2(json2);
+        };
 
-                loadData();
-                }, [props.filename1, props.filename2]);
+        loadData();
+        }, [props.filename1, props.filename2]);
 
-            useEffect(() => {
-                fetch('@/../pkg/testcrate_bg.wasm')
-                .then(response => {
-                    return response.arrayBuffer();
-                    })
-                .then(bytes => {
-                    if (data1 && data2) {
-                        const wasm_binary = wasm_js.initSync(bytes);
-                        const varname = props.varnames.join(",");
-                        const data1js = JSON.stringify(data1);
-                        const data2js = JSON.stringify(data2);
-                        const resultJson = wasm_js.parse_json_add(data1js, data2js, varname, props.nentries);
-                        const resultObj = JSON.parse(resultJson);
-                        setResult(resultObj);
-                    }
-                    })
-                .catch(error => {
-                    console.error('Error fetching wasm module:', error);
-                    });
-                }, [data1, data2, props.varnames, props.nentries]);
+    useEffect(() => {
+        fetch('@/../pkg/testcrate_bg.wasm')
+        .then(response => {
+            return response.arrayBuffer();
+            })
+        .then(bytes => {
+            if (data1 && data2) {
+                const wasm_binary = wasm_js.initSync(bytes);
+                const varname = props.varnames.join(",");
+                const data1js = JSON.stringify(data1);
+                const data2js = JSON.stringify(data2);
+                const resultJson = wasm_js.parse_json_add(data1js, data2js, varname, props.nentries);
+                const resultObj = JSON.parse(resultJson);
+                setResult(resultObj);
+            }
+            })
+        .catch(error => {
+            console.error('Error fetching wasm module:', error);
+            });
+        }, [data1, data2, props.varnames, props.nentries]);
 
-            return (
-                <div className={styles.json}>
-                    <h1>BindGen1</h1>
-                        {result && <pre>{JSON.stringify(result, null, 2)}</pre>}
-                </div>
-            )
+    return (
+        <div className={styles.json}>
+            <h1>BindGen1</h1>
+                {result ? result && <pre>{JSON.stringify(result, null, 2)}</pre> : 'Loading...'}
+        </div>
+    )
+}
 
-        }
-
-        return Component
-    },
+const WasmBindGenCalc = dynamic(() => Promise.resolve(Component), {
     ssr: false
 });
 
