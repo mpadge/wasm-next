37429ff00203fd002ca5783dcc5f93112599bff7
mpadge <mark.padgham@email.com>
2023-12-14 11:36:09 +0100

    refactor WasmVectorMult as client-side component

--- src/components/WasmVectorMult.tsx
+++ src/components/WasmVectorMult.tsx
@@ -1,5 +1,7 @@
-import dynamic from 'next/dynamic'
+'use client'
 
+import { useState, useEffect } from 'react';
+import dynamic from 'next/dynamic'
 import styles from '../styles/styles.module.css';
 
 interface VectorMultProps {
@@ -16,35 +18,50 @@ function allocateSpaceForVector(vector: Float64Array, memory: WebAssembly.Memory
   return ptr;
 }
 
-const WasmVectorMult = dynamic({
-  loader: async () => {
-    // @ts-ignore
-    const { mult_two, get_result_len, memory } = await import('@/../pkg/testcrate_bg.wasm');
+const WasmVectorMultComponent = ({ vector1, vector2 }: VectorMultProps) => {
+    const [mult_two, setMultTwo] = useState<Function | null>(null);
+    const [get_result_len, setGetResultLen] = useState<Function | null>(null);
+    const [memory, setMemory] = useState<Function | null>(null);
 
-    const Component = ({ vector1, vector2 }: VectorMultProps) => {
+    useEffect(() => {
+        const loadWasm = async () => {
+            const wasmModule = await import('@/../pkg/testcrate_bg.wasm');
+            setMultTwo(() => wasmModule.mult_two);
+            setGetResultLen(() => wasmModule.get_result_len);
+            setMemory(() => wasmModule.memory);
+        };
 
-        const ptr1 = allocateSpaceForVector(vector1, memory);
-        const ptr2 = allocateSpaceForVector(vector2, memory);
+        loadWasm();
+    }, []);
 
-        const resultPtr = mult_two(ptr1, vector1.length, ptr2, vector2.length);
-        const resultLen = get_result_len();
+    const [resultVector, setResultVector] = useState<Float64Array | null>(null);
+    useEffect(() => {
+        if (mult_two && get_result_len && memory) {
+            const ptr1 = allocateSpaceForVector(vector1, memory);
+            const ptr2 = allocateSpaceForVector(vector2, memory);
 
-        const resultVector = new Float64Array(memory.buffer, resultPtr, resultLen);
+            const resultPtr = mult_two(ptr1, vector1.length, ptr2, vector2.length);
+            const resultLen = get_result_len();
+            const resultVector = new Float64Array(memory.buffer, resultPtr, resultLen);
+            setResultVector(resultVector);
+        }
+    }, [mult_two, get_result_len, memory, vector1, vector2]);
 
-        return (
-            <div className={styles.vector}>
-              <h1>Vector Result</h1>
-              {Array.from(resultVector).map((value: number, index: number) => (
-                <div key={index}>{value}</div>
-              ))}
-            </div>
-        )
-    }
+    return (
+        <div className={styles.vector}>
+        <h1>Vector Result</h1>
+        {resultVector ?
+            Array.from(resultVector).map((value: number, index: number) => (
+            <div key={index}>{value}</div>)) :
+            'Loading...'
+        }
+        </div>
+    )
+}
 
-    return Component
-    },
-    // Ensure only client-side execution:
-    ssr: false
+const WasmVectorMult = dynamic(() => Promise.resolve(WasmVectorMultComponent), {
+  // Ensure only client-side execution:
+  ssr: false
 })
 
 export default WasmVectorMult
