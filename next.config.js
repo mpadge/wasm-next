7f346e8bbc59a85b876a9c149ef79f78b52dbaeb
mpadge <mark.padgham@email.com>
2024-11-29 12:33:49 +0100

    update next.config, package.json (no consequences here)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config, { isServer, dev }) {
+    webpack ( config, { isServer, dev, webpack }) {
         // Use the client static directory in the server bundle and prod mode
         // Fixes `Error occurred prerendering page "/"`
         config.output.webassemblyModuleFilename =
@@ -10,6 +10,14 @@ const nextConfig = {
 
         // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
         config.experiments = { ...config.experiments, asyncWebAssembly: true }
+
+        // https://nextjs.org/docs/app/building-your-application/optimizing/memory-usage#disable-webpack-cache
+        // This just stops building altogether:
+        // if (config.cache && !dev) {
+        //     config.cache = Object.freeze({
+        //         type: 'memory',
+        //     })
+        // }
 
         // Deubbing (vercel/next.js/issues/27650)
         config.infrastructureLogging = { debug: /PackFileCache/ }
