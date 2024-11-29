d7073e3a4349f2a2e766d09b7fd128062d414a4c
mpadge <mark.padgham@email.com>
2024-11-29 12:09:20 +0100

    activate webpack debugging in next.config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config, { isServer, dev }) {
-    // Use the client static directory in the server bundle and prod mode
-    // Fixes `Error occurred prerendering page "/"`
-    config.output.webassemblyModuleFilename =
-      isServer && !dev
-        ? '../static/pkg/[modulehash].wasm'
-        : 'static/pkg/[modulehash].wasm'
+    webpack(config, { isServer, dev }) {
+        // Use the client static directory in the server bundle and prod mode
+        // Fixes `Error occurred prerendering page "/"`
+        config.output.webassemblyModuleFilename =
+            isServer && !dev
+                ? '../static/pkg/[modulehash].wasm'
+                : 'static/pkg/[modulehash].wasm'
 
-    // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
-    config.experiments = { ...config.experiments, asyncWebAssembly: true }
+        // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
+        config.experiments = { ...config.experiments, asyncWebAssembly: true }
 
-    return config
-  },
+        // Deubbing (vercel/next.js/issues/27650)
+        config.infrastructureLogging = { debug: /PackFileCache/ }
+
+        return config
+    },
 }
 
 module.exports = nextConfig
